094ce123457cef347295dab270dd7925cc7b1945
sinethembabhomba <sinethembabhomba@gmail.com>
2015-07-21 12:00:31 +0200

    Design changed
    I made some changes on a design

--- Sunshine.co.za/app/src/main/java/com/example/sinethembab/sunshinecoza/ForecastFragment.java
+++ Sunshine.co.za/app/src/main/java/com/example/sinethembab/sunshinecoza/ForecastFragment.java
@@ -52,7 +52,7 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
     // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
     // must change.
-    static final int COL_WEATHER_ID = 0;
+    public static final int COL_WEATHER_ID = 0;
     public static final int COL_WEATHER_DATE = 1;
     public static final int COL_WEATHER_DESC = 2;
     public static final int COL_WEATHER_MAX_TEMP = 3;
